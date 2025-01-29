778e6ecea8e75610393b2c1f3f45513ef8ea3418
Matthias <matthiascmurray@gmail.com>
2025-01-29 11:03:01 -0700

    helloworld service added

---
+++ services/helloworld/setup.sh
@@ -0,0 +1,7 @@
+# echo cloning down textcat_goemotions...
+# spacy project clone tutorials/textcat_goemotions
+# echo Downloading spacy project assets...
+# cd textcat_goemotions
+# spacy project assets
+# cd ..
+echo "Saying hello..."
